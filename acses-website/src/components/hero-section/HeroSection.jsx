ea93b185b2cb65d275f6e7e3b91a8b56f4ea53b4
Lawson Buabassah <buabassahlawson@gmail.com>
2025-01-03 14:19:55 +0000

    feat: update hero section

--- acses-website/src/components/hero-section/HeroSection.jsx
+++ acses-website/src/components/hero-section/HeroSection.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const HeroSection = () => {
   return (
-    <section className="w-full flex justify-center py-12 md:py-24 lg:py-32 xl:py-48 bg-black">
+    <section className="w-full flex justify-center py-12 md:py-24 lg:py-32 xl:py-48 bg-acses-green-500">
       <div className="container px-4 md:px-6">
         <div className="grid gap-6 lg:grid-cols-[1fr_400px] lg:gap-12 xl:grid-cols-[1fr_600px]">
           <div className="flex flex-col justify-center space-y-4">
@@ -12,8 +12,7 @@ const HeroSection = () => {
               </h1>
               <p className="max-w-[600px] text-gray-300 md:text-xl">
                 Empowering future tech leaders through innovation,
-                collaboration, and excellence in computer science and
-                engineering.
+                collaboration, and excellence.
               </p>
             </div>
             <div className="flex flex-col gap-2 min-[400px]:flex-row">

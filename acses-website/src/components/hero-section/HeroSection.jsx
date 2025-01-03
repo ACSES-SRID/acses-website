fa857a7d2d7677c23387de2f694a80ad4b7db817
Lawson Buabassah <buabassahlawson@gmail.com>
2025-01-03 14:47:24 +0000

    feat: update hero section

--- acses-website/src/components/hero-section/HeroSection.jsx
+++ acses-website/src/components/hero-section/HeroSection.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const HeroSection = () => {
   return (
-    <section className="w-full flex justify-center py-12 md:py-24 lg:py-32 xl:py-48 bg-acses-green-500">
+    <section className="w-full flex justify-center pt-28 py-12 md:py-24 lg:py-32 xl:py-48 bg-acses-green-500 md:pt-0">
       <div className="container px-4 md:px-6">
         <div className="grid gap-6 lg:grid-cols-[1fr_400px] lg:gap-12 xl:grid-cols-[1fr_600px]">
           <div className="flex flex-col justify-center space-y-4">

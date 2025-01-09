3a866bdffc728aeb7809cd57b8d4bc906aca86eb
NanaYawAustin <yaw292@gmail.com>
2025-01-09 23:39:27 +0000

    Card colopur changes

--- acses-website/src/components/welcome/MessageCard.jsx
+++ acses-website/src/components/welcome/MessageCard.jsx
@@ -2,7 +2,8 @@ import React from "react";
 
 const MessageCard = ({ title, name, imageSrc, message }) => {
   return (
-    <div className="w-full border rounded-lg shadow-md p-6 min-h-[16rem] md:min-h-[20rem] transition-transform transform hover:scale-105 hover:shadow-lg bg-gradient-to-r from-green-300 via-green-200 to-white hover:from-green-400 hover:via-green-300">
+    <div className="w-full border rounded-lg shadow-md p-6 min-h-[16rem] md:min-h-[20rem] transition-transform transform hover:scale-105 hover:shadow-lg bg-acses-green-500 hover:bg-green-400">
+
       <div className="flex items-center gap-4 mb-4">
         <div className="relative w-16 h-16 rounded-full overflow-hidden">
           <img
@@ -17,14 +18,7 @@ const MessageCard = ({ title, name, imageSrc, message }) => {
         </div>
       </div>
       <div>
-      <p
-        className="text-black-6000 font-bold"
-           style={{
-            textShadow: "0.5px 0.5px 1px white, -0.5px -0.5px 1px white",
-         }}
->
-         {message}
-       </p>
+      <p className="text-black-6000 font-bold">{message}</p>
 
       </div>
     </div>

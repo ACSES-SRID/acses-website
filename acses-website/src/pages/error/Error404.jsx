3f63e429b29e024f67cf921c3af4b72a713de6af
Harris-Musah <hrrsabdallah123@gmail.com>
2025-01-24 15:07:02 +0000

    Update on Error404

--- acses-website/src/pages/error/Error404.jsx
+++ acses-website/src/pages/error/Error404.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import logo from '/logo/logo.jpg'
 
 const Error404 = () => {
   return (
-    <div>Error404</div>
+    <>
+      <div className='flex flex-col justify-center items-center w-screen h-screen top-0 left-0'>
+        <img src={logo} className='w-56 h-56'/>
+        <p className='text-3xl text-center'>The page you&apos;re looking for does not exist.</p>
+        <p className='text-center'>click here to go to <a href="/" className=' p-1 text-xl rounded-md text-green-900'>Home page</a></p>
+      </div>
+    </>
+    
   )
 }
 

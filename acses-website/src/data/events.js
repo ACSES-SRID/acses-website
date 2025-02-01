f4758b46fcbbf25d82286f0e9498589a47b5288c
Lawson Buabassah <buabassahlawson@gmail.com>
2025-02-01 09:44:17 +0000

    feat: add event link for general meeting

--- acses-website/src/data/events.js
+++ acses-website/src/data/events.js
@@ -12,7 +12,7 @@ export const events = [
           "Our first General Meeting for the 2024/2025 academic year. This meeting is a gathering of all association members to discuss key topics. First-year students will be oriented to their programs, and faculty heads will also be present.",
         location: "Auditorium",
         type: "meeting",
-        link: '',
+        link: 'https://calendar.app.google/Py61HRSxwFAmZzxv8',
       }
     ],
   }

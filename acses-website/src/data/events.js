fb32b404ae0be1b35025de20e829d5162d0b8eaf
Harris-Musah <hrrsabdallah123@gmail.com>
2025-03-02 12:13:23 +0000

    Added the event bits to bytes 2.0 to the calender

--- acses-website/src/data/events.js
+++ acses-website/src/data/events.js
@@ -12,8 +12,8 @@ export const events = [
           "Our first General Meeting for the 2024/2025 academic year. This meeting is a gathering of all association members to discuss key topics. First-year students will be oriented to their programs, and faculty heads will also be present.",
         location: "Auditorium",
         type: "meeting",
-        link: 'https://calendar.app.google/Py61HRSxwFAmZzxv8',
-      }
+        link: "https://calendar.app.google/Py61HRSxwFAmZzxv8",
+      },
     ],
   },
   {
@@ -28,9 +28,117 @@ export const events = [
           "Strictly a meeting for executives only to discuss matters of the association. This meeting will involve discussions on the state of the association, planning for upcoming events, and making decisions on matters of the association.",
         location: "Computer Lab",
         type: "meeting",
-        link: 'https://calendar.app.google/fjVfsWdesshfKsPq9'
+        link: "https://calendar.app.google/fjVfsWdesshfKsPq9",
+      },
+    ],
+  },
+  {
+    id: 3,
+    date: "07-03-2025",
+    events: [
+      {
+        id: "e1",
+        time: "04:30 PM",
+        title: "Bits to bytes 2.0",
+        description:
+          "this Program provide us with discussions with experts in various tech space",
+        location: "Computer Lab",
+        type: "meeting",
+        link: "https://calendar.app.google/mhiBw9V9Z66Nn3CL6",
+      },
+    ],
+  },
+  {
+    id: 4,
+    date: "07-03-2025",
+    events: [
+      {
+        id: "e1",
+        time: "04:30 PM",
+        title: "Email Workshop",
+        description:
+          "this Program provide us with discussions with experts in various tech space",
+        location: "Computer Lab",
+        type: "meeting",
+        link: "#",
+      },
+    ],
+  },
+  {
+    id: 5,
+    date: "07-03-2025",
+    events: [
+      {
+        id: "e1",
+        time: "04:30 PM",
+        title: "Code Night",
+        description:
+          "An event where talented students display their skills in coding",
+        location: "Computer Lab",
+        type: "meeting",
+        link: "#",
+      },
+    ],
+  },
+  {
+    id: 6,
+    date: "07-03-2025",
+    events: [
+      {
+        id: "e1",
+        time: "04:30 PM",
+        title: "Twitter Space",
+        description: "An event/meeting to help students share their ideas on X",
+        location: "Computer Lab",
+        type: "meeting",
+        link: "#",
+      },
+    ],
+  },
+  {
+    id: 7,
+    date: "07-03-2025",
+    events: [
+      {
+        id: "e1",
+        time: "04:30 PM",
+        title: "E-sports",
+        description: "An online sports events",
+        location: "Computer Lab",
+        type: "meeting",
+        link: "#",
+      },
+    ],
+  },
+  {
+    id: 8,
+    date: "07-03-2025",
+    events: [
+      {
+        id: "e1",
+        time: "04:30 PM",
+        title: "Inter level Tech and Innovations Display",
+        description: "A competition between student from each year group",
+        location: "Computer Lab",
+        type: "meeting",
+        link: "#",
+      },
+    ],
+  },
+  {
+    id: 9,
+    date: "07-03-2025",
+    events: [
+      {
+        id: "e1",
+        time: "04:30 PM",
+        title: "Second General meeting",
+        description:
+          "To discuss difficulties members faced during the semester and the upcoming exams E",
+        location: "Computer Lab",
+        type: "meeting",
+        link: "#",
       },
     ],
   },
 ];
-
